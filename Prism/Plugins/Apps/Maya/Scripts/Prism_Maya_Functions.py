2786f41568be0cc85c189bb6becb3635ad90628c
RichardFrangenberg <richardf.1@web.de>
2019-06-03 16:23:53 +0100

    maya unit convert fix

--- Prism/Plugins/Apps/Maya/Scripts/Prism_Maya_Functions.py
+++ Prism/Plugins/Apps/Maya/Scripts/Prism_Maya_Functions.py
@@ -742,8 +742,10 @@ class Prism_Maya_Functions(object):
 							scaleAnimated = True
 							break
 
+					cmds.move(0, 0, 0, i + '.scalePivot', i + '.rotatePivot', absolute=True)
+
 					for k in ["x", "y", "z"]:
-						for m in [".t", ".s"]:
+						for m in [".s"]:
 							connections = cmds.listConnections(i + m + k, c=True, plugs=True)
 							if connections is not None:
 								ucNode = cmds.createNode("unitConversion")
