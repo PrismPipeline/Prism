01f66110cc43b6ae99b9a75337157f93972ec477
RichardFrangenberg <richardf.1@web.de>
2019-12-11 01:20:54 +1030

    version bump

--- Prism/Scripts/PrismCore.py
+++ Prism/Scripts/PrismCore.py
@@ -122,7 +122,7 @@ class PrismCore:
 
         try:
             # set some general variables
-            self.version = "v1.2.1.40"
+            self.version = "v1.2.1.41"
 
             self.prismRoot = os.path.abspath(
                 os.path.dirname(os.path.dirname(__file__))
